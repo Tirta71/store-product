917422734ffee415b1e4384647a7dcc57a58f398
tirta71 <tirta4132@gmail.com>
2023-05-24 19:45:00 +0700

    Cape

--- src/pages/register/register.js
+++ src/pages/register/register.js
@@ -1,16 +1,30 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { Spinner } from "react-bootstrap";
 import "../login/login.css";
+import { toast, ToastContainer } from "react-toastify";
+
 const Register = () => {
   const navigate = useNavigate();
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleRegister = () => {
     if (password !== confirmPassword) {
-      setError("Passwords do not match");
+      setError("Password tidak cocok");
+      return;
+    }
+
+    if (!username || !password || !confirmPassword) {
+      toast.error("input tidak boleh kosong");
+      return;
+    }
+
+    if (!/^[A-Z][A-Za-z0-9]{7,}$/.test(password)) {
+      toast.error("Password Harus Kapital dan 8 Huruf");
       return;
     }
 
@@ -18,23 +32,33 @@ const Register = () => {
     const userExists = existingUsers.find((user) => user.username === username);
 
     if (userExists) {
-      setError("Username Sudah ada");
-    } else if (password !== confirmPassword) {
-      setError("Passwords Tidak Cocok");
+      setError("Username already exists");
     } else {
-      const newUser = { username, password, cartItems: [] };
-      existingUsers.push(newUser);
-      localStorage.setItem("users", JSON.stringify(existingUsers));
-      navigate("/login");
+      setIsLoading(true);
+
+      setTimeout(() => {
+        const newUser = { username, password, cartItems: [] };
+        existingUsers.push(newUser);
+        localStorage.setItem("users", JSON.stringify(existingUsers));
+        navigate("/login");
+        setIsLoading(false);
+
+        setTimeout(function () {
+          toast.success("Register Berhasil");
+        }, 1000);
+      }, 2000);
     }
   };
+
   const handleKeyPress = (e) => {
     if (e.key === "Enter") {
       handleRegister();
     }
   };
+
   return (
     <div className="login-container">
+      <ToastContainer />
       <div className="form">
         <h1>Register</h1>
         <div className="control">
@@ -62,8 +86,25 @@ const Register = () => {
             onKeyPress={handleKeyPress}
           />
         </div>
-        <button className="btn" onClick={handleRegister}>
-          Register
+        <button
+          className="register-button"
+          onClick={handleRegister}
+          disabled={isLoading}
+        >
+          {isLoading ? (
+            <>
+              <Spinner
+                as="span"
+                animation="border"
+                size="sm"
+                role="status"
+                aria-hidden="true"
+              />
+              <span className="sr-only">Loading...</span>
+            </>
+          ) : (
+            "Register"
+          )}
         </button>
         {error && <p className="error-message">{error}</p>}
         <span>
