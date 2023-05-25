48210689263631b1ab1d0ba1718b1b045d7e07ee
tirta71 <tirta4132@gmail.com>
2023-05-26 03:18:52 +0700

    yeyyy

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 
+import "react-toastify/dist/ReactToastify.css";
+
 import ProductsComponent from "../component/product";
 import "../CSS/home.css";
 import NavbarReusable from "../component/navbarReusable";
@@ -7,25 +9,62 @@ import NavbarReusable from "../component/navbarReusable";
 const Home = ({ addToCart, removeFromCart }) => {
   const [cartItems, setCartItems] = useState([]);
   const [showContent, setShowContent] = useState(false);
+  const [loggingOut, setLoggingOut] = useState(false);
 
   useEffect(() => {
     const storedCartItems = JSON.parse(localStorage.getItem("cartItems")) || [];
     setCartItems(storedCartItems);
 
+    let timeout;
+
+    const resetTimeout = () => {
+      clearTimeout(timeout);
+      timeout = setTimeout(() => {
+        handleLogout();
+      }, 60000);
+    };
+
+    document.addEventListener("mousemove", resetTimeout);
+    document.addEventListener("keydown", resetTimeout);
+    document.addEventListener("click", resetTimeout);
+
+    return () => {
+      clearTimeout(timeout);
+      document.removeEventListener("mousemove", resetTimeout);
+      document.removeEventListener("keydown", resetTimeout);
+      document.removeEventListener("click", resetTimeout);
+    };
+  }, []);
+
+  const handleLogout = () => {
+    setLoggingOut(true);
+
+    setTimeout(() => {
+      Object.keys(localStorage).forEach((key) => {
+        if (key !== "users" && key !== "stokProduk") {
+          localStorage.removeItem(key);
+        }
+      });
+      setLoggingOut(false);
+      setTimeout(() => {
+        window.location.href = "/";
+      }, 2000);
+    }, 2000);
+  };
+
+  useEffect(() => {
     setTimeout(() => {
       setShowContent(true);
     }, 500);
   }, []);
 
   return (
-    <div
-      className={`up-transisi container mt-4 ${
-        showContent ? "show-content" : " "
-      } `}
-    >
+    <div className="container mt-4">
       <NavbarReusable />
-      <h1 className="text-center mt-5">Daftar Produk</h1>
-      <div className="card-container">
+      <h1 className={`text-center mt-5 ${showContent ? "show-title" : ""}`}>
+        Daftar Produk
+      </h1>
+      <div className={`card-container ${showContent ? "show-content" : ""}`}>
         <ProductsComponent addToCart={addToCart} />
       </div>
     </div>
